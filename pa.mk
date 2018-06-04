1ec855a50ef1bd2e34a44f4e62470c833c03db13
Vincent Zvikaramba <zvikovincent@gmail.com>
2018-06-04 01:49:55 -0400

    j5nlte: Add PA Makefile
    Change-Id: I510275c02825132c81aa32f25bfa8e38bb8d2ad9

---
+++ pa.mk
@@ -0,0 +1,15 @@
+
+# Inherit from common
+$(call inherit-product, device/samsung/j5-common/pa.mk)
+
+$(call inherit-product, device/samsung/j5nlte/device.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := j5nlte
+PRODUCT_NAME := pa_j5nlte
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-J500FN
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_CHARACTERISTICS := phone
+
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
